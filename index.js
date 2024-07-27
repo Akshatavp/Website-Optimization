baa68e2b657f68a63b6ea9157b43fc9d808f717f
Dhanushchandra <Dhanushbharath9@gmail.com>
2024-07-27 22:58:12 +0530

    Preliminary assessment of high-volume GET and POST requests

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const sendMessage = require("./producer");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Request counter and timestamp tracking
+let requestCount = 0;
+let startTime = Date.now();
+
+app.post("/register", (req, res) => {
+  requestCount++;
+  const registrationData = req.body;
+  sendMessage(registrationData);
+  res.status(202).send("Registration request received.");
+});
+
+app.get("/", (req, res) => {
+  requestCount++;
+  res.json({
+    message: "I am Alive",
+  });
+});
+
+// Periodically log the request rate
+setInterval(() => {
+  const currentTime = Date.now();
+  const elapsedSeconds = (currentTime - startTime) / 1000;
+  console.log(`Requests per second: ${requestCount / elapsedSeconds}`);
+  requestCount = 0; // Reset counter
+  startTime = currentTime; // Reset start time
+}, 1000); // Log every second
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ producer.js
@@ -0,0 +1,27 @@
+const kafka = require('kafka-node');
+const Producer = kafka.Producer;
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = new Producer(client);
+
+producer.on('ready', () => {
+  console.log('Kafka Producer is connected and ready.');
+});
+
+producer.on('error', (error) => {
+  console.error('Producer error:', error);
+});
+
+const sendMessage = (message) => {
+  const payloads = [
+    { topic: 'registration-topic', messages: JSON.stringify(message), partition: 0 },
+  ];
+  producer.send(payloads, (error, data) => {
+    if (error) {
+      console.error('Failed to send message:', error);
+    } else {
+    //   console.log('Message sent:', data);
+    }
+  });
+};
+
+module.exports = sendMessage;
